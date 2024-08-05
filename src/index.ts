69c38b22a148aad4bb74a32d472b1e5058b392c6
wzc520pyfm <1528857653@qq.com>
2024-08-05 23:19:00 +0800

    feat: add sucrase

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,65 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import process from 'node:process'
+import { transform } from 'sucrase'
+import { createFilter } from '@rollup/pluginutils'
 import type { UnpluginFactory } from 'unplugin'
 import { createUnplugin } from 'unplugin'
 import type { Options } from './types'
 
-export const unpluginFactory: UnpluginFactory<Options | undefined> = options => ({
-  name: 'unplugin-starter',
-  transformInclude(id) {
-    return id.endsWith('main.ts')
-  },
-  transform(code) {
-    return code.replace('__UNPLUGIN__', `Hello Unplugin! ${options}`)
-  },
-})
+export const unpluginFactory: UnpluginFactory<Options | undefined> = (options = {} as any) => {
+  const filter = createFilter(options.include, options.exclude)
+
+  return {
+    name: 'unplugin-sucrase',
+    resolveId(importee, importer) {
+      if (importer && /^[./]/.test(importee)) {
+        const resolved = path.resolve(importer ? path.dirname(importer) : process.cwd(), importee)
+        // resolve in the same order that TypeScript resolves modules
+        const resolvedFilenames = [
+          `${resolved}.ts`,
+          `${resolved}.tsx`,
+          `${resolved}/index.ts`,
+          `${resolved}/index.tsx`,
+        ]
+        if (resolved.endsWith('.js')) {
+          resolvedFilenames.splice(
+            2,
+            0,
+            `${resolved.slice(0, -3)}.ts`,
+            `${resolved.slice(0, -3)}.tsx`,
+          )
+        }
+        const resolvedFilename = resolvedFilenames.find(filename => fs.existsSync(filename))
+
+        if (resolvedFilename)
+          return resolvedFilename
+      }
+    },
+    transform(code, id) {
+      if (!filter(id))
+        return null
+
+      const result = transform(code, {
+        transforms: options.transforms!,
+        jsxPragma: options.jsxPragma,
+        jsxFragmentPragma: options.jsxFragmentPragma,
+        enableLegacyTypeScriptModuleInterop: options.enableLegacyTypeScriptModuleInterop,
+        enableLegacyBabel5ModuleInterop: options.enableLegacyBabel5ModuleInterop,
+        production: options.production,
+        disableESTransforms: options.disableESTransforms,
+        filePath: id,
+        sourceMapOptions: {
+          compiledFilename: id,
+        },
+      })
+      return {
+        code: result.code,
+        map: result.sourceMap,
+      }
+    },
+  }
+}
 
 export const unplugin = /* #__PURE__ */ createUnplugin(unpluginFactory)
 
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,12 @@
-export interface Options {
+import type { Options as SucraseOptions } from 'sucrase'
+
+/**
+ * A valid `picomatch` glob pattern, or array of patterns.
+ */
+export type FilterPattern = ReadonlyArray<string | RegExp> | string | RegExp | null
+
+export interface Options extends Partial<SucraseOptions> {
   // define your plugin options here
+  include?: FilterPattern
+  exclude?: FilterPattern
 }
